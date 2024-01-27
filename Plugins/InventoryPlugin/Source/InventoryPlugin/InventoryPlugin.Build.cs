859c8e8520d0b99b939f31658f50645c1d7789a8
Nartyna <mplutwska@gmail.com>
2024-01-27 20:04:59 +0100

    New Version, updated 2 plugins, added stylized egypt asset pack

--- Plugins/InventoryPlugin/Source/InventoryPlugin/InventoryPlugin.Build.cs
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/InventoryPlugin.Build.cs
@@ -38,6 +38,7 @@ public class InventoryPlugin : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"GameplayTags",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
